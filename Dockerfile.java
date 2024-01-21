10cb16e5792b787cd41b561d82842c2a9bcd8360
GecaMelisa <melisa.geca@stu.ibu.edu.ba>
2024-01-21 19:02:23 +0100

    Deployment

--- Dockerfile.java
+++
@@ -1,9 +0,0 @@
-        FROM maven:3.8.5-openjdk-17-slim as build
-        COPY . .
-        RUN mvn clean package -DskipTests
-
-        FROM openjdk:17.0.1-jdk-slim
-        COPY --from=build /target/job-search-platform-0.0.1-SNAPSHOT.jar job-search-platform.jar
-        EXPOSE 8080
-
-        ENTRYPOINT ["java","-jar","job-search-platform.jar"]
